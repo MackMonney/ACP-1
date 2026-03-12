d414d65921b571230ed4ee9413599d06b24cdd8b
Monney Christopher <monneychristopher4197@gmail.com>
2026-03-12 18:45:01 +0000

    File Handling part 2

---
+++ FileHandling_2.py
@@ -0,0 +1,15 @@
+#open file and read its content
+file = open('Codingal.txt', 'r')
+print(file.read())
+file.close()
+
+#open file and read its beginning 8 characters
+file = open('Codingal.txt', 'r')
+print('\n Read in parts \n')
+print(file.read(8))
+file.close()
+
+#append your name and age in the file
+file = open('Codingal.txt', 'r')
+file.write('\n I am Penguin and I am 1 year old')
+file.close()
