4a623d1aa1a4ea01288e1d346256c9bd0ae67bc0
Monney Christopher <monneychristopher4197@gmail.com>
2026-04-01 19:15:53 +0000

    Conversion of tuple to list

---
+++ tupletolist.py
@@ -0,0 +1,9 @@
+# Storing personal details in a tuple
+personal_details = ("Mack", "Monney", 20, 1.79, 53.3, "Mathematics")
+
+# Converting tuple to list
+personal_list = list(personal_details)
+
+# Display results
+print("Tuple:", personal_details)
+print("List:", personal_list)
